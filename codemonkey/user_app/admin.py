5a87519f3d924979b6804e1e711f830818ce9dbf
Tyler Davis <tylerdavis@mail.usf.edu>
2018-05-23 20:02:41 -0400

    1.041
    update

--- codemonkey/user_app/admin.py
+++ codemonkey/user_app/admin.py
@@ -2,11 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-<<<<<<< HEAD
+
 from user_app.models import User
 # Register your models here.
 admin.site.register(User)
-=======
+
 
 # Register your models here.
->>>>>>> 8a4f83386a4af3f8031d964a91f6a5b7b443f896
